b34ca1603a9da5968a4f8ba7bf0761f7be2883f4
Francesco Di Maria <dimaria.francesco@outlook.it>
2021-09-08 16:07:10 +0200

    update

--- exercise-19.js
+++ exercise-19.js
@@ -5,9 +5,6 @@ const person1 = {
 };
 
 const person2 = person1;
-
-// console.log(person1); 
-// Se io  dichiaro un console.log prima del di assegnare un nuovo valore per person2 il risultato sarà sempre quello contenuto dentro person1
 person2.firstName = 'Simon';
 
 console.log(person1);
